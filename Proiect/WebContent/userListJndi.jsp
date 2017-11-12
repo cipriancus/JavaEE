99ba16c68102191a3259504c75d8e2f17350b5f4
cocageorgiana <cocageorgiana@gmail.com>
2017-11-12 19:31:21 +0200

    jndi

--- Proiect/WebContent/userListJndi.jsp
+++ Proiect/WebContent/userListJndi.jsp
@@ -1,12 +1,30 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/sql" prefix="sql" %>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/sql" prefix="sql"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-    
-<sql:query var="listUsers" dataSource="jdbc/UsersDB">
+	pageEncoding="UTF-8"%>
+<%@ page import="com.java.jndi.operation.UserListServlet"%>
+<%
+	//String name = request.getAttribute("name");
+	Object name = request.getSession().getAttribute("name");
+	String nCountry = String.valueOf(name);
+	out.println("Attribute Value : " + nCountry);
+%>
+<c:set var="nm" value="<%=nCountry%>" />
+<c:if test="${(nm == 'US') }">
+
+	<sql:query var="listFr" dataSource="jdbc/UsersFr">
 	select student_name, student_email, student_password, student_gender,student_address from student_record;
 </sql:query>
-    
+</c:if>
+
+<c:set var="nm" value="<%=nCountry%>" />
+<c:if test="${(nm == 'Romania') }">
+
+	<sql:query var="listRo" dataSource="jdbc/UsersRo">
+	select student_name, student_email, student_password, student_gender,student_address from student_record;
+</sql:query>
+</c:if>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
 	"http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -17,7 +35,9 @@
 <body>
 	<div align="center">
 		<table border="1" cellpadding="5">
-			<caption><h2>List of users</h2></caption>
+			<caption>
+				<h2>List of users</h2>
+			</caption>
 			<tr>
 				<th>Name</th>
 				<th>Email</th>
@@ -25,15 +45,31 @@
 				<th>Gender</th>
 				<th>Address</th>
 			</tr>
-			<c:forEach var="user" items="${listUsers.rows}">
-				<tr>
-					<td><c:out value="${user.student_name}" /></td>
-					<td><c:out value="${user.student_email}" /></td>
-					<td><c:out value="${user.student_password}" /></td>
-					<td><c:out value="${user.student_gender}" /></td>
-					<td><c:out value="${user.student_address}" /></td>
-				</tr>
-			</c:forEach>
+			<c:choose>
+				<c:when test="${nm=='France'}">
+					<c:forEach var="user" items="${listFr.rows}">
+						<tr>
+							<td><c:out value="${user.student_name}" /></td>
+							<td><c:out value="${user.student_email}" /></td>
+							<td><c:out value="${user.student_password}" /></td>
+							<td><c:out value="${user.student_gender}" /></td>
+							<td><c:out value="${user.student_address}" /></td>
+						</tr>
+					</c:forEach>
+					<br />
+				</c:when>
+				<c:otherwise>
+					<c:forEach var="user" items="${listRo.rows}">
+						<tr>
+							<td><c:out value="${user.student_name}" /></td>
+							<td><c:out value="${user.student_email}" /></td>
+							<td><c:out value="${user.student_password}" /></td>
+							<td><c:out value="${user.student_gender}" /></td>
+							<td><c:out value="${user.student_address}" /></td>
+						</tr>
+					</c:forEach>
+				</c:otherwise>
+			</c:choose>
 		</table>
 	</div>
 </body>
--- Proiect/src/com/java/jndi/operation/UserListServlet.java
+++ Proiect/src/com/java/jndi/operation/UserListServlet.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Locale;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -17,35 +18,83 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+
+import javax.servlet.http.*;
+import javax.servlet.http.HttpSession;
+
 @WebServlet("/listUsers")
 public class UserListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Locale usa = new Locale("", "US");
+		usa.toString().substring(1, 3);
+		System.out.println(usa.getDisplayCountry());
+		Locale romania = new Locale("", "RO");
+		System.out.println(romania.getDisplayCountry());
 		PrintWriter writer = response.getWriter();
-		try {
-			Context initContext = new InitialContext();
-			Context envContext = (Context) initContext.lookup("java:comp/env");
-			DataSource ds = (DataSource) envContext.lookup("jdbc/UsersDB");
-			Connection conn = ds.getConnection();
-			
-			Statement statement = conn.createStatement();
-			String sql = "select student_name, student_email, student_password, student_gender,"
-					+ "student_address from student_record";
-			ResultSet rs = statement.executeQuery(sql);
-			
-			int count = 1;
-			while (rs.next()) {
-				writer.println(String.format("User #%d: %-15s %s", count++, 
-						rs.getString("student_name"), rs.getString("student_email"), rs.getString("student_password"), 
-						rs.getString("student_gender"), rs.getString("student_address")));
-				
+
+		HttpSession session = request.getSession(true);
+
+		if ((usa.toString().substring(1, 3)).equals(request.getLocale().getCountry())) {
+
+			session.setAttribute("name", usa.toString().substring(1, 3));
+
+			RequestDispatcher errorDispatcher = request.getRequestDispatcher("/WEB-INF/userListJndi.jsp");
+			errorDispatcher.forward(request, response);
+			try {
+				Context initContext = new InitialContext();
+				Context envContext = (Context) initContext.lookup("java:comp/env");
+				DataSource ds = (DataSource) envContext.lookup("jdbc/UsersFr");
+				Connection conn = ds.getConnection();
+
+				Statement statement = conn.createStatement();
+
+				String sql = "select student_name, student_email, student_password, student_gender,"
+						+ "student_address from student_record";
+				ResultSet rs = statement.executeQuery(sql);
+
+				int count = 1;
+				while (rs.next()) {
+					writer.println(String.format("User #%d: %-15s %s", count++, rs.getString("student_name"),
+							rs.getString("student_email"), rs.getString("student_password"),
+							rs.getString("student_gender"), rs.getString("student_address")));
+
+				}
+			} catch (NamingException ex) {
+				System.err.println(ex);
+			} catch (SQLException ex) {
+				System.err.println(ex);
 			}
-		} catch (NamingException ex) {
-			System.err.println(ex);
-		} catch (SQLException ex) {
-			System.err.println(ex);
+		} else if (romania.equals(request.getLocale().getCountry())) {
+			Object name = request.getAttribute("name");
+			try {
+				Context initContext = new InitialContext();
+				Context envContext = (Context) initContext.lookup("java:comp/env");
+				DataSource ds = (DataSource) envContext.lookup("jdbc/UsersRo");
+				Connection conn = ds.getConnection();
+
+				Statement statement = conn.createStatement();
+
+				String sql = "select student_name, student_email, student_password, student_gender,"
+						+ "student_address from student_record";
+				ResultSet rs = statement.executeQuery(sql);
+
+				int count = 1;
+				while (rs.next()) {
+					writer.println(String.format("User #%d: %-15s %s", count++, rs.getString("student_name"),
+							rs.getString("student_email"), rs.getString("student_password"),
+							rs.getString("student_gender"), rs.getString("student_address")));
+
+				}
+			} catch (NamingException ex) {
+				System.err.println(ex);
+			} catch (SQLException ex) {
+				System.err.println(ex);
+			}
 		}
 	}
 
